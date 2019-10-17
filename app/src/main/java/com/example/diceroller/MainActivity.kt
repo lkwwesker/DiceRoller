5d79c07a240d875d7c8cfcf787440c5b72da8fe2
Kah Weng <lkwwesker@gmail.com>
2019-10-18 00:47:59 +0800

    second commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -14,7 +14,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = this.findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-
+        val countButton: Button= this.findViewById(R.id.count_button)
+        countButton.setOnClickListener{countDice()}
     }
 
     private fun rollDice() {
@@ -23,14 +24,51 @@ class MainActivity : AppCompatActivity() {
         val resultText: TextView = findViewById(R.id.result_text)
         val resultText1: TextView = findViewById(R.id.result_text1)
         val resultText2: TextView = findViewById(R.id.result_text2)
-        resultText.text = "Dice Rolled!"
-        resultText1.text=""
-        resultText2.text=""
+        resultText.text = ""
+        resultText1.text = ""
+        resultText2.text = ""
+
         val randomInt = Random().nextInt(6)+ 1
         resultText.text = randomInt.toString()
         val randomInt1 = Random().nextInt(6)+ 1
         resultText1.text = randomInt1.toString()
         val randomInt2 = Random().nextInt(6)+ 1
         resultText2.text = randomInt2.toString()
+    }
+    private fun countDice() {
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+        val resultText1: TextView = findViewById(R.id.result_text1)
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        var rt = resultText.getText().toString()
+        var rt1 = resultText1.getText().toString()
+        var rt2 = resultText2.getText().toString()
+        var d1 = rt.toInt()
+        var d2 = rt1.toInt()
+        var d3 = rt2.toInt()
+
+        val randomInt = Random().nextInt(6)+ 1
+        d1 += randomInt
+        if(d1<=6)
+        resultText.text = d1.toString()
+        else
+            resultText.text = "6"
+
+        val randomInt1 = Random().nextInt(6)+ 1
+        d2 += randomInt1
+        if(d2<=6)
+            resultText1.text = d1.toString()
+        else
+            resultText1.text = "6"
+
+        val randomInt2 = Random().nextInt(6)+ 1
+        d3 += randomInt2
+        if(d3<=6)
+            resultText2.text = d1.toString()
+        else
+            resultText2.text = "6"
+
+
     }
 }
